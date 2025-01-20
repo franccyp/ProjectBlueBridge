54fc84419c464e458de18cb2e4d7cfd4dfbf7989
LindaIzadi <s0578461@htw-berlin.de>
2025-01-21 00:40:19 +0100

    Hilfe und projekteinzelansicht aus der leiste rausgenommen

--- menu.php
+++ menu.php
@@ -6,8 +6,6 @@ class Menu {
         echo '<div class="sidebar">';
         echo '<ul>';
         echo '<li><a href="get_projects_test.php">Projektübersicht</a></li>';
-        echo '<li><a href="projekteinzelansicht.php">Projekteinzelansicht</a></li>';
-        echo '<li><a href="hilfe.php">Hilfe</a></li>';
         echo '</ul>';
         echo '</div>';
     }
