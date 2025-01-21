2b3963cc8893887f79059d6e4857563fcc3f5a38
LindaIzadi <s0578461@htw-berlin.de>
2025-01-21 01:27:49 +0100

    Projektübersicht Ansicht geändert

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'is_li/project-blue-bridge',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b19cb2b2c7724a660ec77a11ce1b780a3aa628c7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'is_li/project-blue-bridge' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b19cb2b2c7724a660ec77a11ce1b780a3aa628c7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
