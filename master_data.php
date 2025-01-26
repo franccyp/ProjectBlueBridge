0951501a31c21319e410470ebbcf76867cc0029b
LindaIzadi <s0578461@htw-berlin.de>
2025-01-27 00:29:44 +0100

    vollständig

--- master_data.php
+++ master_data.php
@@ -2,7 +2,7 @@
 
 class MasterDataResolver {
     private $baseUrl = "https://dashboard-examples.blueant.cloud/rest/v1/masterdata/projects";
-    private $authHeader = "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3OTkzMjYxMDAiLCJpYXQiOjE3MzQwOTAzNjAsImlzcyI6IkJsdWUgQW50wqkiLCJleHAiOjE4OTE3NzAzNjB9.efWIguMIVfA24nz2Jp7NWEgBJiTWEBaN_hXUbff1Dfk";
+    private $authHeader = "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3OTkzMjYwOTIiLCJpYXQiOjE3MzE3ODgwMjIsImlzcyI6IkJsdWUgQW50wqkiLCJleHAiOjE4ODk0NjgwMjJ9.iEU7DX_APQ-s2nfEgBZpe7BPIP0SxtL3N4tNwUh8ioU";
     private $resolvedData = [
         'statuses' => [],
         'types' => [],
